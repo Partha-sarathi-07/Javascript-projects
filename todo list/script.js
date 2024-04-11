adb8836295a272f6a075970a66bd7105260f6249
Partha-sarathi-07 <sarathicharu07@gmail.com>
2024-04-11 12:18:19 +0530

    final

--- todo list/script.js
+++ todo list/script.js
@@ -1,10 +1,10 @@
-const inputBox=document.getElementById("task");
+const inputBox = document.getElementById("task");
 const listContainer = document.getElementById("list-container");
 
-function addtask(){
-  if(inputBox.value === '')
+function addtask() {
+  if (inputBox.value === '')
     alert("Enter your work to add Buddy....");
-  else{
+  else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value + '<span>&#x2717;</span>';
     listContainer.appendChild(li);
@@ -13,17 +13,16 @@ function addtask(){
   saveData();
 }
 
-listContainer.addEventListener("click", function(e)
-{
-  if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
     saveData();
   }
-  else if(e.target.tagName === "SPAN"){
+  else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
     saveData();
   }
-},false);
+}, false);
 
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML); // Consistent casing
